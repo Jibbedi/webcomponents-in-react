8a3f98e94b1486206189b2b51befcde19a45cfc1
Johannes Kling <johannes.kling92@gmail.com>
2019-06-08 23:50:25 +0200

    added support for rich data

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,48 @@ export const adapt = (
     } | null = {};
 
     componentDidMount() {
+      this.setUpEventListeners();
+      this.updatePropertiesForRichData();
+    }
+
+    componentDidUpdate() {
+      this.updatePropertiesForRichData();
+    }
+
+    componentWillUnmount() {
+      this.removeEventListeners();
+    }
+
+    mapKeyToEventName(key: string) {
+      if (this.shouldKeyBeMapped(key)) {
+        return overrideProps![key];
+      }
+
+      // onChange -> change
+      return key.substr(2).toLowerCase();
+    }
+
+    mapKeyToPropertyName(key: string) {
+      if (this.shouldKeyBeMapped(key)) {
+        return overrideProps![key];
+      }
+
+      return key;
+    }
+
+    shouldKeyBeMapped(key: string) {
+      return overrideProps ? !!overrideProps[key] : false;
+    }
+
+    removeEventListeners() {
+      for (let key in this.eventListeners!) {
+        const handler = this.eventListeners![key];
+        this.ref!.removeEventListener(this.mapKeyToEventName(key), handler);
+      }
+      this.eventListeners = null;
+    }
+
+    setUpEventListeners() {
       for (let key in this.props) {
         const handler = this.props[key];
 
@@ -31,31 +73,37 @@ export const adapt = (
       }
     }
 
-    componentWillUnmount() {
-      for (let key in this.eventListeners!) {
-        const handler = this.eventListeners![key];
-        this.ref!.removeEventListener(this.mapKeyToEventName(key), handler);
+    updatePropertiesForRichData() {
+      for (let key in this.props) {
+        const data = this.props[key];
+        if (!this.isRichData(data)) {
+          continue;
+        }
+        (this.ref as any)![this.mapKeyToPropertyName(key)] = data;
       }
-      this.eventListeners = null;
     }
 
-    mapKeyToEventName(key: string) {
-      if (this.shouldKeyBeMapped(key)) {
-        return overrideProps![key];
-      }
-
-      // onChange -> change
-      return key.substr(2).toLowerCase();
+    filterOutRichData(props: any) {
+      return Object.keys(props).reduce(
+        (filteredProps, propKey) => {
+          const value = props[propKey];
+          if (!this.isRichData(value)) {
+            filteredProps[propKey] = value;
+          }
+          return filteredProps;
+        },
+        {} as any
+      );
     }
 
-    shouldKeyBeMapped(key: string) {
-      return overrideProps ? !!overrideProps[key] : false;
+    isRichData(prop: any) {
+      return typeof prop === "object";
     }
 
     render() {
       return React.createElement(componentSelector, {
         ref: (ref: HTMLElement) => (this.ref = ref),
-        ...this.props
+        ...this.filterOutRichData(this.props)
       });
     }
   };
